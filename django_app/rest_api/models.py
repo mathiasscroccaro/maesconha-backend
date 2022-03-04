6f33563df7d4aa24528ba8b78e28fee1e78eef08
Mathias Scroccaro Costa <mathias.scroccaro@gmail.com>
2022-03-03 23:52:32 -0300

    Added create test for User model

--- django_app/rest_api/models.py
+++ django_app/rest_api/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseUser(models.Model):
+    first_name = models.CharField(
+        max_length=30,
+        blank=False
+    )
+    last_name = models.CharField(
+        max_length=100,
+        blank=False
+    )
+    birth = models.DateField()
+    patient = models.BooleanField(default=False)
+
+
+class AssociatedUser(BaseUser):
+    pass
+
+
+class DependentUser(BaseUser):
+    associated_user = models.ForeignKey(
+        AssociatedUser, 
+        on_delete=models.CASCADE,
+        related_name='dependent_users'
+    )
+
+    def __init__(self):
+        patient = True
+
+
+class Document(models.Model):
+   pass 
--- django_app/rest_api/tests/test_models.py
+++ django_app/rest_api/tests/test_models.py
@@ -3,9 +3,38 @@ from django.test import TestCase
 from rest_api.models import AssociatedUser
 from rest_api.models import DependentUser
 from rest_api.models import Document
+from rest_api.models import BaseUser
+
+import datetime
 
 
-class AssociatedUser(TestCase):
+class AssociatedUserTest(TestCase):
+    def test_create(self):
+        date = datetime.datetime(1994, 7, 7)
+        u = AssociatedUser(
+            first_name='Mathias',
+            last_name='Scroccaro Costa',
+            birth=date,
+            patient=True
+        )
+        u.save()
+        self.assertEqual(AssociatedUser.objects.count(), 1)
+
+        users = AssociatedUser.objects.all()
+        user = users[0]
+        self.assertEqual(user.first_name, 'Mathias')
+        self.assertEqual(user.last_name, 'Scroccaro Costa')
+        self.assertEqual(user.birth, date.date())
+
+    def test_modify(self):
+
+        pass
+
+    def test_create_and_delete(self):
+        pass
+
+
+class DependentUserTest(TestCase):
     def test_create(self):
         pass
 
@@ -16,18 +45,7 @@ class AssociatedUser(TestCase):
         pass
 
 
-class DependentUser(TestCase):
-    def test_create(self):
-        pass
-
-    def test_modify(self):
-        pass
-
-    def test_create_and_delete(self):
-        pass
-
-
-class Document(TestCase):
+class DocumentTest(TestCase):
     def test_create(self):
         pass
 
